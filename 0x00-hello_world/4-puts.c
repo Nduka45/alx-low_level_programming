e8f864c311565d68fc219b64d1baf89d865eff02
Nduka45 <mcsunny4real@gmail.com>
2023-08-10 18:35:47 -0700

    Hello_c_programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point	"print the puzzle" 
+ *
+ *
+ * Return: Always 0 (pass)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point "proper grammar"
+ *               
+ *
+ * Return: Always 0 (pass)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
