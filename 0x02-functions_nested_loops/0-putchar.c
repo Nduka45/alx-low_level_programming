eff9cfcd85b99aca8ac957ec7cf9df761a7c4f68
Nduka45 <mcsunny4real@gmail.com>
2023-08-17 08:37:30 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  *main - Entry point
+  *Description: 'Check for number if it is positive or negative'
+  *Return: always 0
+  */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
