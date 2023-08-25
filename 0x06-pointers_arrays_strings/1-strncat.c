d26bc98a0d28d7e15f11b2323c1eaa5ec33999c7
Nduka45 <mcsunny4real@gmail.com>
2023-08-25 08:01:46 -0700

    string conat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,12 +3,13 @@
 
 /**
  * _strcat - concatenates two strings
- * @dest: 
+ * @dest: dest
  * @src: string to add
+ * @n: number
  *
  * Return: a pointer to the resulting string
  */
-char *_strncat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-	return (strncat(dest, src));
+	return (strncat(dest, src, n));
 }
