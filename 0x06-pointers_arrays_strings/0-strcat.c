d47ea73085d2b1a0487eb4cb1b2a2d52d4f46c88
Nduka45 <mcsunny4real@gmail.com>
2023-08-26 05:36:33 -0700

    string concat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: dest
+ * @src: string to add
+ *
+ * Return: a pointer to the resulting string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
