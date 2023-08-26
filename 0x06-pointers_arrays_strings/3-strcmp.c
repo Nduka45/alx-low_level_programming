fbe592402da522b37ed1f682ee44afbc89d34476
Nduka45 <mcsunny4real@gmail.com>
2023-08-26 06:17:27 -0700

    3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - copies a string
+ * @s1: value 1
+ * @s2: value 2
+ *
+ * Return: a pointer to the resulting string
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
